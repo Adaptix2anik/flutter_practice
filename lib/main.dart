85875b79904b43f7260eb6278f52204b26e056de
Adaptix2anik <aniketlahamg23@gmail.com>
2019-12-18 23:37:01 +0530

    practice

--- lib/main.dart
+++ lib/main.dart
@@ -18,18 +18,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -57,41 +46,58 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have clicked the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: new _myList(),
     );
   }
+}
+
+class _myList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new ListView.builder(
+        padding: new EdgeInsets.all(4.0),
+        itemBuilder: (context,i){
+          return new ListTile(
+            title : new Text(
+              'hello', style: new TextStyle(fontStyle: FontStyle.italic, color: Colors.orange),
+            ),
+            subtitle: new Text('this is subtitle'),
+            leading: new Icon(Icons.face),
+            trailing: new RaisedButton(
+              child: new Text('Remove'),
+              onPressed: (){
+                deleteDialog(buildContext).then((onValue){
+                  
+                }
+              },
+            ),
+          );
+        },
+    );
+  }
+}
+
+Future<bool> deleteDialog(BuildContext buildContext){
+  return showDialog(
+    context: buildContext,
+    builder: (BuildContext context){
+      return new AlertDialog(
+        title: new Text('Are You Sure'),
+        actions: <Widget>[
+          new FlatButton(
+            child: new Text('Yes'),
+            onPressed: (){
+              Navigator.of(context).pop(true);
+            },
+          ),
+          new FlatButton(
+            child: new Text('No'),
+            onPressed: (){
+              Navigator.of(context).pop(false);
+            },
+          )
+        ],
+      );
+    }
+  );
 }
